df40579dc7ef980010edee56301ad9a540a1b46f
Nicolas Fortunato <nicolas.fortunato@mercadolibre.com>
2018-01-08 18:48:35 -0300

    initial package

---
+++ sdk/src/main/java/com/mercadopago/reviewconfirm/ReviewConfirmActivity.java
@@ -0,0 +1,10 @@
+package com.mercadopago.reviewconfirm;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nfortuna on 1/8/18.
+ */
+
+public class ReviewConfirmActivity extends AppCompatActivity {
+}
