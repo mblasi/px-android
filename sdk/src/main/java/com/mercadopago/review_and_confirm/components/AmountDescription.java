a771b951d85e6fcd6a195194e4b89c5e3656ab02
matiasromar <matias.romar@mercadolibre>
2018-03-02 10:22:24 -0300

    added amountDescriptionRenderer

--- sdk/src/main/java/com/mercadopago/review_and_confirm/components/AmountDescription.java
+++ sdk/src/main/java/com/mercadopago/review_and_confirm/components/AmountDescription.java
@@ -3,14 +3,15 @@ package com.mercadopago.review_and_confirm.components;
 import android.support.annotation.NonNull;
 
 import com.mercadopago.components.Component;
+import com.mercadopago.review_and_confirm.props.AmountDescriptionProps;
 
 /**
  * Created by mromar on 2/28/18.
  */
 
-public class AmountDescription extends Component<AmountDescription> {
+public class AmountDescription extends Component<AmountDescriptionProps, Void> {
 
-    public AmountDescription(@NonNull Object props) {
+    public AmountDescription(@NonNull AmountDescriptionProps props) {
         super(props);
     }
 }
--- sdk/src/main/java/com/mercadopago/review_and_confirm/components/AmountDescriptionRenderer.java
+++ sdk/src/main/java/com/mercadopago/review_and_confirm/components/AmountDescriptionRenderer.java
@@ -1,12 +1,16 @@
 package com.mercadopago.review_and_confirm.components;
 
 import android.content.Context;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.mercadopago.R;
 import com.mercadopago.components.Renderer;
 import com.mercadopago.customviews.MPTextView;
+import com.mercadopago.util.CurrenciesUtil;
+
+import java.math.BigDecimal;
 
 /**
  * Created by mromar on 2/28/18.
@@ -20,9 +24,16 @@ public class AmountDescriptionRenderer extends Renderer<AmountDescription> {
         final MPTextView descriptionTextView = bodyView.findViewById(R.id.mpsdkDescription);
         final MPTextView amountTextView = bodyView.findViewById(R.id.mpsdkAmount);
 
-        setText(descriptionTextView, component.getDescription());
-        setText(amountTextView, component.getAmount());
+        setText(descriptionTextView, component.props.description);
+        setText(amountTextView, getFormattedAmount(component.props.amount, component.props.currencyId));
+
+        descriptionTextView.setTextColor(component.props.textColor);
+        amountTextView.setTextColor(component.props.textColor);
 
         return bodyView;
+    }
+
+    private Spanned getFormattedAmount(BigDecimal amount, String currencyId) {
+        return CurrenciesUtil.getFormattedAmount(amount, currencyId);
     }
 }
