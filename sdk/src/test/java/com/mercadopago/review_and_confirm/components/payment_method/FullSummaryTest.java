86788830224aa2ba69c49da04a3d30bc0e87befa
matiasromar <matias.romar@mercadolibre>
2018-03-12 11:48:38 -0300

    added dimensions in compactSummary xml

---
+++ sdk/src/test/java/com/mercadopago/review_and_confirm/components/payment_method/FullSummaryTest.java
@@ -0,0 +1,16 @@
+package com.mercadopago.review_and_confirm.components.payment_method;
+
+/**
+ * Created by mromar on 3/12/18.
+ */
+
+public class FullSummaryTest {
+
+    /*
+    @Mock
+    PaymentModel model;
+
+    @Mock
+    PaymentMethodComponent.Actions actions;
+    */
+}
