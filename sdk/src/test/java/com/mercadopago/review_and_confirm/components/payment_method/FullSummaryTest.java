4c55eff4ff25ddb65b8b5a8f04b034c9dde4d363
matiasromar <matias.romar@mercadolibre>
2018-03-12 14:05:45 -0300

    added test cases

--- sdk/src/test/java/com/mercadopago/review_and_confirm/components/payment_method/FullSummaryTest.java
+++ sdk/src/test/java/com/mercadopago/review_and_confirm/components/payment_method/FullSummaryTest.java
@@ -1,16 +1,101 @@
 package com.mercadopago.review_and_confirm.components.payment_method;
 
+import com.mercadopago.constants.PaymentTypes;
+import com.mercadopago.model.Discount;
+import com.mercadopago.model.PayerCost;
+import com.mercadopago.model.PaymentMethod;
+import com.mercadopago.model.Site;
+import com.mercadopago.review_and_confirm.SummaryProviderImpl;
+import com.mercadopago.review_and_confirm.components.FullSummary;
+import com.mercadopago.review_and_confirm.models.SummaryModel;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+
 /**
  * Created by mromar on 3/12/18.
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class FullSummaryTest {
 
-    /*
-    @Mock
-    PaymentModel model;
+    private final static String CURRENCY_ID = "ARS";
+    private final static Long DISCOUNT_ID = 77L;
+    private final static BigDecimal DISCOUNT_COUPON_AMOUNT = new BigDecimal(20);
+    private final static BigDecimal DISCOUNT_PERCENT_OFF = new BigDecimal(20);
+
+    private final static BigDecimal TOTAL_AMOUNT = new BigDecimal(1000);
+    private final static Site SITE = new Site("MLA", CURRENCY_ID);
+    private final static PayerCost PAYER_COST = new PayerCost();
+
+    private final static String PAYMENT_TYPE_ID = PaymentTypes.CREDIT_CARD;
 
     @Mock
-    PaymentMethodComponent.Actions actions;
-    */
+    SummaryProviderImpl provider;
+
+    @Test
+    public void whenGetTotalAmountThenGetTotalAmountWithoutDiscount() throws Exception {
+        SummaryModel model = new SummaryModel(TOTAL_AMOUNT, getPaymentMethod(), SITE, getPayerCostWithoutInstallments(), null, null);
+
+        FullSummary component = new FullSummary(model, provider);
+
+        Assert.assertEquals(component.getTotalAmount(), PAYER_COST.getTotalAmount());
+    }
+
+    @Test
+    public void whenGetTotalAmountThenGetTotalAmountWithDiscount() throws Exception {
+        SummaryModel model = new SummaryModel(TOTAL_AMOUNT, getPaymentMethod(), SITE, getPayerCostWithDiscount(), getDiscount(), null);
+
+        FullSummary component = new FullSummary(model, provider);
+
+        Assert.assertEquals(component.getTotalAmount(), getTotalAmountWithDiscount());
+    }
+
+
+    private PaymentMethod getPaymentMethod() {
+        PaymentMethod paymentMethod = new PaymentMethod();
+        paymentMethod.setPaymentTypeId(PAYMENT_TYPE_ID);
+
+        return paymentMethod;
+    }
+
+    private PayerCost getPayerCostWithoutInstallments() {
+        PayerCost payerCost = new PayerCost();
+
+        payerCost.setTotalAmount(TOTAL_AMOUNT);
+        payerCost.setInstallments(1);
+        payerCost.setInstallmentRate(new BigDecimal(0));
+
+        return payerCost;
+    }
+
+    private PayerCost getPayerCostWithDiscount() {
+        PayerCost payerCost = new PayerCost();
+
+        payerCost.setTotalAmount(getTotalAmountWithDiscount());
+        payerCost.setInstallments(1);
+        payerCost.setInstallmentRate(new BigDecimal(0));
+
+        return payerCost;
+    }
+
+    private Discount getDiscount() {
+        Discount discount = new Discount();
+
+        discount.setId(DISCOUNT_ID);
+        discount.setCouponAmount(DISCOUNT_COUPON_AMOUNT);
+        discount.setCurrencyId(CURRENCY_ID);
+        discount.setPercentOff(DISCOUNT_PERCENT_OFF);
+
+        return discount;
+    }
+
+    private BigDecimal getTotalAmountWithDiscount() {
+        return TOTAL_AMOUNT.subtract(getDiscount().getCouponAmount());
+    }
 }
